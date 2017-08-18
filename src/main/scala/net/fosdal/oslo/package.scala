7a077f7de5abd7eb4bae6afc5d46d949930cc644
Steve Fosdal <steve@fosdal.net>
2017-08-18 13:12:49 -0700

    combined oany and base package

--- src/main/scala/net/fosdal/oslo/package.scala
+++ src/main/scala/net/fosdal/oslo/package.scala
@@ -79,6 +79,20 @@ package object oslo extends Oslo {
     pollUntil(config)(block)
   }
 
+  implicit class AnyOps[A](val a: A) extends AnyVal {
+
+    def partialTap(pf: PartialFunction[A, _]): A = {
+      pf.lift(a)
+      a
+    }
+
+    def tap(f: Function[A, _]): A = {
+      f(a)
+      a
+    }
+
+  }
+
 }
 
 trait Oslo {
--- src/test/scala/net/fosdal/oslo/OsloSpec.scala
+++ src/test/scala/net/fosdal/oslo/OsloSpec.scala
@@ -192,6 +192,64 @@ class OsloSpec extends WordSpec with Matchers with PropertyChecks with OsloMatch
 
   }
 
+  "tap" must {
+    "operate on the supplied instance and return it when complete" in new TapFixture {
+      val touchable = Touchable("unused")
+      touchable should not be 'touched
+      var didIt = false
+      val tappedTouchable: Touchable = touchable tap { (t: Touchable) =>
+        didIt = true
+        t shouldBe theSameInstanceAs(touchable)
+        t.touch()
+        t shouldBe 'touched
+      }
+      didIt           shouldBe true
+      tappedTouchable shouldBe theSameInstanceAs(touchable)
+      touchable       shouldBe 'touched
+    }
+  }
+
+  "partialTap" must {
+    "operate on the supplied instance only if it is covered by the partial function and then return it when complete" in new TapFixture {
+      val touchable = Touchable("covered")
+      touchable should not be 'touched
+      var didIt = false
+      val tappedTouchable: Touchable = touchable partialTap {
+        case (t: Touchable) if t.id == "covered" =>
+          didIt = true
+          t shouldBe theSameInstanceAs(touchable)
+          t.touch()
+          t shouldBe 'touched
+      }
+      didIt           shouldBe true
+      tappedTouchable shouldBe theSameInstanceAs(touchable)
+      touchable       shouldBe 'touched
+    }
+    "not operate on the supplied instance if it is not covered by the partial function and then return it when complete" in new TapFixture {
+      val touchable = Touchable("not covered")
+      touchable should not be 'touched
+      var didIt = false
+      val tappedTouchable: Touchable = touchable partialTap {
+        case (t: Touchable) if t.id == "covered" =>
+          didIt = true
+          t shouldBe theSameInstanceAs(touchable)
+          t.touch()
+          t shouldBe 'touched
+      }
+      didIt           shouldBe false
+      tappedTouchable shouldBe theSameInstanceAs(touchable)
+      touchable should not be 'touched
+    }
+  }
+
+  trait TapFixture {
+    case class Touchable(id: String) {
+      var isTouched = false
+      def touch(): Unit = isTouched = true
+      def reset(): Unit = isTouched = false
+    }
+  }
+
   trait UsingFixture {
 
     class UsingNoOpable
