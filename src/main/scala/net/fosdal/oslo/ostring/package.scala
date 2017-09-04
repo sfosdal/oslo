353ca151753fbdea82da3cf11dfdae917dd5cdda
Steve Fosdal <steve@fosdal.net>
2017-09-03 21:06:35 -0700

    adding asBytes

--- src/main/scala/net/fosdal/oslo/ostring/package.scala
+++ src/main/scala/net/fosdal/oslo/ostring/package.scala
@@ -1,14 +1,40 @@
 package net.fosdal.oslo
 
+import scala.math.pow
+
 package object ostring {
 
-  private[this] val notAlpha   = """[^a-zA-Z]"""
-  private[this] val notNumeric = """[^0-9]"""
+  private[this] val NotAlpha        = "[^a-zA-Z]"
+  private[this] val NotNumeric      = "[^0-9]"
+  private[this] val NotAlphaNumeric = "[^a-zA-Z0-9]"
+
+  private[this] val ByteFactors = Seq("b", "kb", "mb", "gb", "tb", "pb", "eb").zipWithIndex
+    .map(t => t._1 -> pow(1024D, t._2.toDouble))
+    .toMap
+  private[this] val BytePattern = "([0-9.]+?)\\s*([yzeptgmk]?b)".r
 
   implicit class StringOps(val s: String) extends AnyVal {
-    def toAlpha: String        = s.replaceAll(notAlpha, "")
-    def toNumeric: String      = s.replaceAll(notNumeric, "")
-    def toAlphanumeric: String = s.replaceAll(notAlpha, "").replaceAll(notNumeric, "")
+
+    def toAlpha: String = s.replaceAll(NotAlpha, "")
+
+    def toNumeric: String = s.replaceAll(NotNumeric, "")
+
+    def toAlphanumeric: String = s.replaceAll(NotAlphaNumeric, "")
+
+    def asBytes: Long = {
+      s.toLowerCase.trim match {
+        case BytePattern(n, u) =>
+          val d = n.trim.toDouble * ByteFactors(u.trim)
+          if (math.floor(d) == d) {
+            d.toLong
+          } else {
+            throw new NumberFormatException
+          }
+        case _ =>
+          s.toLong
+      }
+    }
+
   }
 
 }
--- src/test/scala/net/fosdal/oslo/ostring/OStringSpec.scala
+++ src/test/scala/net/fosdal/oslo/ostring/OStringSpec.scala
@@ -3,7 +3,7 @@ package net.fosdal.oslo.ostring
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen._
 import org.scalatest.prop.PropertyChecks
-import org.scalatest.{Ignore, Matchers, WordSpec}
+import org.scalatest.{Matchers, WordSpec}
 
 class OStringSpec extends WordSpec with Matchers with PropertyChecks {
 
@@ -53,6 +53,49 @@ class OStringSpec extends WordSpec with Matchers with PropertyChecks {
       "return an empty string" in new Fixture {
         Seq(arbString.arbitrary, alphaStr, alphaNumStr, numStr) foreach { gen =>
           forAll(gen)(_.replaceAll(alphanumeric, "").toAlphanumeric shouldBe 'empty)
+        }
+      }
+    }
+  }
+
+  "asBytes" must {
+    "parse standard formats properly" in {
+      "1b".asBytes     shouldBe 1L
+      "2000b".asBytes  shouldBe 2000L
+      "1.5kb".asBytes  shouldBe 1536L
+      "2000kb".asBytes shouldBe 2048000L
+      "1.5mb".asBytes  shouldBe 1572864L
+      "2000mb".asBytes shouldBe 2097152000L
+      "1.5gb".asBytes  shouldBe 1610612736L
+      "2000gb".asBytes shouldBe 2147483648000L
+      "1.5tb".asBytes  shouldBe 1649267441664L
+      "2000tb".asBytes shouldBe 2199023255552000L
+      "1.5pb".asBytes  shouldBe 1688849860263936L
+      "2000pb".asBytes shouldBe 2251799813685248000L
+      "1.5eb".asBytes  shouldBe 1729382256910270464L
+      "2000eb".asBytes shouldBe 9223372036854775807L
+      "8eb".asBytes    shouldBe 9223372036854775807L
+    }
+    "be forgiving" in {
+      "  1  b  ".asBytes shouldBe 1L
+    }
+    "fall back to toLong" in {
+      "9".asBytes shouldBe 9L
+    }
+    "fail properly" when {
+      "given unknown units" in {
+        intercept[NumberFormatException] {
+          "9xxx".asBytes
+        }
+      }
+      "given decimal bytes" in {
+        intercept[NumberFormatException] {
+          "9.1".asBytes
+        }
+      }
+      "given decimal kilobytes" in {
+        intercept[NumberFormatException] {
+          "9.1001kb".asBytes
         }
       }
     }
